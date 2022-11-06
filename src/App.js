344028556d5509b4039a4fc584a643b70a579b20
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-06 05:59:48 +0300

    start calendar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar/calendar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -37,7 +37,7 @@ const App = () => {
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
